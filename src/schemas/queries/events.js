f74289847dac7726be1769a1b28900ebf29007ce
Tyler Williams <tyler.williams@footlocker.com>
2019-06-24 22:30:38 -0500

    Fix events query

--- src/schemas/queries/events.js
+++ src/schemas/queries/events.js
@@ -7,10 +7,8 @@ const getEvents = async args => {
 	return [
 		...((status === 200) && events.map(({id, taxonomies, ...rest}) => ({
 			id,
-			// isConcert: taxonomies.some(({name}) => name === 'concert'),
-			// isSport: taxonomies.some(({name}) => name === 'sports'),
-			isConcert: false,
-			isSport: false,
+			isConcert: taxonomies.some(({name}) => name === 'concert'),
+			isSport: taxonomies.some(({name}) => name === 'sports'),
 			time: rest.datetime_local
 		})))
 	]
