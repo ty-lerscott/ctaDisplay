e91c14044af9d99cd34018aec3230d34172ca632
Tyler Williams <tyler.williams@footlocker.com>
2019-06-25 08:41:28 -0500

    Fix weather endpoint

--- src/endpoints.js
+++ src/endpoints.js
@@ -17,7 +17,7 @@ const getBusArrivals = ({
 
 const getServiceAlerts = () => 'http://lapi.transitchicago.com/api/1.0/alerts.aspx?outputType=JSON';
 
-const getWeather = () => `http://api.openweathermap.org/data/2.5/weather?appid=${process.env.API_KEY_WEATHER}&zip=90001`;
+const getWeather = () => `http://api.openweathermap.org/data/2.5/weather?appid=${process.env.API_KEY_WEATHER}&zip=60640`;
 
 const getEvents = () => `https://api.seatgeek.com/2/events?venue.id=11&client_id=${process.env.API_KEY_SEAT_GEEK}`;
 
